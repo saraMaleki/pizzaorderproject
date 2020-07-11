0a08864f465be73800a22164f987393c8daf7c78
SaraMaleki <sarah.maleki83@gmail.com>
2020-07-11 16:04:41 +0430

    logout admin

--- pizzaapp/urls.py
+++ pizzaapp/urls.py
@@ -1,11 +1,12 @@
 from django.contrib import admin
 from django.urls import path
-from .views import adminloginview, adminhomepageview,authenticateadmin
+from .views import adminloginview, adminhomepageview,authenticateadmin,logoutadmin
 
 
 urlpatterns = [
 	path('admin/', adminloginview, name='adminloginpage'),
 	path('adminauthenticate/', authenticateadmin, name='authenticateadmin'),
-	path('admin/homepage/', adminhomepageview, name='adminhomepage')
+	path('admin/homepage/', adminhomepageview, name='adminhomepage'),
+	path('adminlogout/', logoutadmin, name='logoutadmin'),
 
 ]
--- pizzaapp/views.py
+++ pizzaapp/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
 
-from django.contrib.auth import authenticate, login
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
 # Create your views here.
 
 def adminloginview(request):
@@ -13,12 +14,18 @@ def authenticateadmin(request):
 
 	user = authenticate(username = username, password=password)
 
-	if user is not None:
+	if user is not None and user.username=='admin':
 		login(request,user)
 		return redirect('adminhomepage')
 
 	if user is None:
+		messages.add_message(request, messages.ERROR, "invalid credentials")
 		return redirect('adminloginpage')
 
 def adminhomepageview(request):
 	return render(request,'adminhomepage.html')
+
+
+def logoutadmin(request):
+	logout(request)
+	return redirect('adminloginpage')
